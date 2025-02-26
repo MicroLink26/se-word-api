04f933e0566f5deb748581a3d6c7861ddc797130
Mike <mic.roche@gmail.com>
2025-02-26 10:52:52 +0100

    rename routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: Mike mic.roche@gmail.com
  * @Date: 2023-08-08 20:46:17
  * @LastEditors: Mike mic.roche@gmail.com
- * @LastEditTime: 2025-02-20 12:06:10
+ * @LastEditTime: 2025-02-20 16:04:47
  * @FilePath: /se-substantiv-api/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,10 +23,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -40,4 +37,5 @@ app.use("/api/nouns", nounRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(process.env.MONGODB_URI);
 });
